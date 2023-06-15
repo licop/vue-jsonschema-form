5a3a066937ad7b9ca770e4d726fc5fed61e3f3d0
licop <licop@users.noreply.github.com>
2023-06-15 17:09:02 +0800

    feat: 创建初始项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+
+export default defineComponent({
+  name: 'Hello World',
+  props: {
+    msg: String
+  },
+  setup(props) {
+    
+  }
+})
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp, defineComponent, h } from 'vue'
+import App from './App.vue'
+
+
+createApp(App).mount('#app')
